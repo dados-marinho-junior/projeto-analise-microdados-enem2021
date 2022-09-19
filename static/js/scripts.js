710df4e520592e6041e7ce5b474c507710b91b97
diogoray <104396832+diogoray@users.noreply.github.com>
2022-09-19 19:51:15 -0300

    static

---
+++ static/js/scripts.js
@@ -0,0 +1,39 @@
+$( document ).ready(function() {
+
+    var baseUrl   = 'http://localhost:8000/';
+    var deleteBtn = $('.delete-btn');
+    var searchBtn = $('#search-btn');
+    var searchForm = $('#search-form');
+    var filter     = $('#filter');
+    
+    $(deleteBtn).on('click', function(e) {
+
+        e.preventDefault();
+
+        var delLink = $(this).attr('href');
+        var result = confirm('Quer deletar esta tarefa?');
+
+        if(result) {
+            window.location.href = delLink;
+        }
+
+    });
+
+    $(searchBtn).on('click', function() {
+        searchForm.submit();
+    });
+
+    $(filter).change(function() {
+        var filter = $(this).val();
+        window.location.href = baseUrl + '?filter=' + filter;
+    });
+
+});
+
+ 
+
+var href = window.location.pathname;
+
+if( $(".navbar-nav a[href='"+href+"']").length ) {
+    $(".navbar-nav a[href='"+href+"']").parent('li').addClass('active');
+}
